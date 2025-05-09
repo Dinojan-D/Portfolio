133201a755329e3b09995c779c35c14f6aa34c2d
Dinojan-D <dinojan212006@gmail.com>
2025-05-09 14:52:56 +0200

    counter

--- src/app/components/top-bar/ViewsDisplay.tsx
+++ src/app/components/top-bar/ViewsDisplay.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
 import { IoMdEye } from "react-icons/io";
 
-
 const ViewsDisplay = () => {
-  return (
-    <div  className= 'h-[32px]  w-min  border-2 border-white rounded-full flex justify-around items-center ' >
-        <IoMdEye className='h-[20px] w-[20px] ml-2 mr-1 ' />
-        <p className='text-center font-semibold mr-2  ml-1' >100</p>
-    </div>
-  )
-}
+  const [views, setViews] = useState<number | null>(null);
 
-export default ViewsDisplay
+  useEffect(() => {
+    fetch('https://api.countapi.xyz/hit/mon-site/visites')
+      .then(res => res.json())
+      .then(data => setViews(data.value))
+      .catch(err => {
+        console.error('Erreur compteur:', err);
+      });
+  }, []);
+
+  return (
+    <div className='h-[32px] w-min border-2 border-white rounded-full flex justify-around items-center'>
+      <IoMdEye className='h-[20px] w-[20px] ml-2 mr-1' />
+      <p className='text-center font-semibold mr-2 ml-1'>
+        {views !== null ? views : '...'}
+      </p>
+    </div>
+  );
+};
+
+export default ViewsDisplay;
