a7d73461f76a4c1ae28aa78b8eaff3b24c7f8097
Dinojan-D <dinojan212006@gmail.com>
2025-05-09 18:56:20 +0200

    counter

--- src/app/components/top-bar/TopBar.tsx
+++ src/app/components/top-bar/TopBar.tsx
@@ -5,8 +5,8 @@ import LangSwitcher from './LangSwitcher'
 const TopBar = () => {
   return (
     <div className=' h-[45px] w-full radius-full  flex justify-center items-center space-x-2 text-[#ffff] rounded-b-xl shadow-lg shadow-black bg-gradient-to-r from-[#1d0327] via-[#56155c] to-[#1d0327] ' >
-      <ViewsDisplay/>
-      <div className='bg-white h-[70%] w-[3px] rounded-full ' ></div>
+      {/*<ViewsDisplay/>
+      <div className='bg-white h-[70%] w-[3px] rounded-full ' ></div>*/}
       <LangSwitcher/>
       <div className='bg-white h-[70%] w-[3px] rounded-full ' ></div>
       <SoundControler/>
--- src/app/components/top-bar/ViewsDisplay.tsx
+++ src/app/components/top-bar/ViewsDisplay.tsx
@@ -2,29 +2,14 @@
 import React from 'react'
 import { IoMdEye } from "react-icons/io";
 
-import { useEffect, useState } from 'react';
 
 const ViewsDisplay = () => {
-  const [uniqueVisitors, setUniqueVisitors] = useState(null);
-
-  useEffect(() => {
-    const fetchVisitorCount = async () => {
-      const res = await fetch('/_vercel/insights/unique-visitors');
-      const data = await res.json();
-      setUniqueVisitors(data.count);
-    };
-
-    fetchVisitorCount();
-  }, []);
-
   return (
-    <div className="h-[32px] w-min border-2 border-white rounded-full flex justify-around items-center">
-      <IoMdEye className="h-[20px] w-[20px] ml-2 mr-1" />
-      <p className="text-center font-semibold mr-2 ml-1">
-        {uniqueVisitors !== null ? `${uniqueVisitors} visiteurs uniques` : 'Chargement...'}
-      </p>
+    <div  className= 'h-[32px]  w-min  border-2 border-white rounded-full flex justify-around items-center ' >
+        <IoMdEye className='h-[20px] w-[20px] ml-2 mr-1 ' />
+        <p className='text-center font-semibold mr-2  ml-1' >00</p>
     </div>
-  );
-};
+  )
+}
 
-export default ViewsDisplay;
+export default ViewsDisplay
